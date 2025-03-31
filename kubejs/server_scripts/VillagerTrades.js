e7d2c6842a409ae72523cc5c8e948754cf6865ab
Andrew Coffman <andrewcoffman679@gmail.com>
2025-03-31 22:51:57 +0000

    added some youngs mods and started work on custom villager traids

---
+++ kubejs/server_scripts/VillagerTrades.js
@@ -0,0 +1,10 @@
+const structureTrade = VillagerUtils.createStructureMapTrade([TradeItem.of("minecraft:emerald", 14, 14), "minecraft:compas"], "minecraft:ancient_city");
+
+MoreJSEvents.villagerTrades((event) => {
+    /**
+     * - `trade`: The trade to add. Read the wiki for more information.
+     *
+     * `addTrade` returns the trade to set optional data.
+     */
+    event.addTrade("minecraft:cartographer", 5, structureTrade);
+});
