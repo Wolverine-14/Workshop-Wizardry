1ae87b8925fbfee6101c622de67021a797d2a34c
Andrew Coffman <andrewcoffman679@gmail.com>
2025-04-14 17:31:26 +0000

    added some villager map traids and updated some mods to be server only. they can be downloaded on the client with the '-s both' tag

--- kubejs/server_scripts/VillagerTrades.js
+++ kubejs/server_scripts/VillagerTrades.js
@@ -1,4 +1,4 @@
-const structureTrade = VillagerUtils.createStructureMapTrade([TradeItem.of("minecraft:emerald", 14, 14), 'minecraft:compass'], "minecraft:ancient_city");
+const structureTrade = VillagerUtils.createStructureMapTrade([TradeItem.of("minecraft:emerald", 14, 14), 'minecraft:compass'], ['minecraft:ancient_city', 'minecraft:pillager_outpost', 'minecraft:igloo', 'betterdeserttemples:desert_temple', 'betterjungletemples:jungle_temple']);
 
 MoreJSEvents.villagerTrades((event) => {
     /**
